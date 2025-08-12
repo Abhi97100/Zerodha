22b42c4e77b8a8ce3d4023721a00fd0c83839a40
Abhi97100 <160091539+Abhi97100@users.noreply.github.com>
2025-08-13 01:11:03 +0530

    Update WatchList.jsx

--- dashboard/src/components/WatchList.jsx
+++ dashboard/src/components/WatchList.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from "react";
 
-import axios from "axios";
+
 
 import GeneralContext from "./GeneralContext.jsx";
 
@@ -179,4 +179,5 @@ const WatchListActions = ({ uid }) => {
       </span>
     </span>
   );
+
 };
